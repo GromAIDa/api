55a478e3cf24ededba465fa608fc6aae779bcfba
hryhorii-opanasenko <hryhorii.opanasenko@gmail.com>
2022-04-07 22:44:30 +0300

    feature/GromAIDa testing webhook

--- app/controllers/transaction.js
+++ app/controllers/transaction.js
@@ -75,21 +75,21 @@ exports.createTransactionInCurrency = async (req, res) => {
     const event = req.body;
     console.log(event);
     // Handle the event
-    switch (event.type) {
-      case 'payment_intent.succeeded':
-        const paymentIntent = event.data.object;
-        console.log('PaymentIntent was successful!');
-        break;
-      case 'payment_method.attached':
-        const paymentMethod = event.data.object;
-        console.log('PaymentMethod was attached to a Customer!');
-        break;
-      // ... handle other event types
-      default:
-        console.log(`Unhandled event type ${event.type}`);
-    }
+    // switch (event.type) {
+    //   case 'payment_intent.succeeded':
+    //     const paymentIntent = event.data.object;
+    //     console.log('PaymentIntent was successful!');
+    //     break;
+    //   case 'payment_method.attached':
+    //     const paymentMethod = event.data.object;
+    //     console.log('PaymentMethod was attached to a Customer!');
+    //     break;
+    //   // ... handle other event types
+    //   default:
+    //     console.log(`Unhandled event type ${event.type}`);
+    // }
 
     // Return a 200 response to acknowledge receipt of the event
-    res.status(StatusCodes.OK).send(event);
+    res.json({ received: true });
   }
 };
