dacff1ad906c8d6653d7d7703dcf2a3c74ee7cdc
hryhorii-opanasenko <hryhorii.opanasenko@gmail.com>
2022-04-11 23:13:58 +0300

    feature/GromAIDa fixed response for getting url

--- app/controllers/transaction.js
+++ app/controllers/transaction.js
@@ -117,7 +117,7 @@ exports.createPaymentLink = async (req, res) => {
       success_url,
       cancel_url,
     });
-    res.status(StatusCodes.OK).send(session.url);
+    res.status(StatusCodes.OK).send({ url: session.url });
   }
 };
 
